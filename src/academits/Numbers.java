089b59ed494b168441204b198326972430ed496b
Наталья Браун <fire_in_heart@mail.ru>
2025-02-25 15:50:01 +0700

    Update Numbers.java

--- src/academits/Numbers.java
+++ src/academits/Numbers.java
@@ -9,28 +9,20 @@ public class Numbers {
         System.out.println("Введите целое число: ");
         int a = scanner.nextInt();
         int sum = 0;
+        int oddSum = 0;
+        int max = 0;
 
         while (a != 0) {
             sum += (a % 10);
+            if ((a % 10) % 2 != 0) {
+                oddSum += (a % 10);
+            }
+            if (a % 10 > max) max = (int) (a % 10);
             a /= 10;
         }
 
         System.out.println("Сумма цифр числа: " + sum);
-
-        int oddSum = 0;
-        while (a != 0) {
-            if ((a % 10) % 2 != 0) {
-                oddSum += (a % 10);
-            }
-            a /= 10;
-        }
         System.out.println("Сумма нечетных цифр числа: " + oddSum);
-
-        int max = 0;
-        while (a != 0) {
-            if (a % 10 > max) max = (int) (a % 10);
-            a /= 10;
-        }
         System.out.println("Максимальная цифра числа = " + max);
     }
 }
