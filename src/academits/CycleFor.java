425a2b52dbbefbb9e4f2274d41d64fe93c86e6e5
Наталья Браун <fire_in_heart@mail.ru>
2025-02-25 15:48:53 +0700

    Update CycleFor.java

--- src/academits/CycleFor.java
+++ src/academits/CycleFor.java
@@ -1,18 +1,20 @@
 package academits;
+
 import java.util.Scanner;
 
 public class CycleFor {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        {
-            for (int i = 100; i >= 1; i--) {
-                if (i % 4 == 0)
 
-                    System.out.print(i + " ");
+        for (int i = 100; i >= 1; i--) {
+            if (i % 4 == 0) {
+                System.out.print(i + " ");
             }
         }
+
+        boolean even = isMultipleFour();
+    }
+
+    private static boolean isMultipleFour() {
+        return false;
     }
 }
-
-
-
