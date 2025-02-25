513103dbdd494e38bc8f8e40168c62fb7bd785a5
Наталья Браун <fire_in_heart@mail.ru>
2025-02-25 15:48:09 +0700

    Update Average.java

--- src/academits/Average.java
+++ src/academits/Average.java
@@ -11,22 +11,20 @@ public class Average {
 
         System.out.println("Введите конец диапазона: ");
         double b = scanner.nextInt();
-
         double sum = 0;
         double qty = 0;
-        for (double i = a; i <= b; i++) {
-            sum += i;
-            qty++;
-        }
         double evenSum = 0;
         double evenQty = 0;
 
         for (double i = a; i <= b; i++) {
+            sum += i;
+            qty++;
             if (i % 2 == 0) {
                 evenQty++;
                 evenSum += i;
             }
         }
+
         double average = getAverage(sum, qty);
         System.out.println("Среднее арифметическое диапазона = " + average);
 
